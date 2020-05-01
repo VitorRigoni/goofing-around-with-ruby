eda4eea18bfe65a3d08e5b14b0c1c60b4f209eca
Vitor Rigoni <vitorrigoni@yahoo.com.br>
2020-05-01 00:47:28 -0700

    Minor docs

--- main.rb
+++ main.rb
@@ -5,6 +5,10 @@ require 'dry/monads/result'
 include Dry::Monads[:try]
 include Dry::Monads[:result]
 
+# Getting data from the Star Wars API and printing the name
+# Cool thing: I found this dry/monads gem. Still very new and lacking tons
+# of stuff, but we have base things like a result type, try, either, validation, and stuff
+# probably enough to get this working =)
 get('https://swapi.dev/api/people/1')
   .bind { |response| Try { response.parsed_response } }
   .to_result
